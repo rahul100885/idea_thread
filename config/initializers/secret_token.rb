2878fa74674d5b95feb771168e20f0c63c68f819
Rahul P. Chaudhari <rahul100885@gmail.com>
2012-05-15 19:55:11 +0530

    Basic app with authenticaltion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaThread::Application.config.secret_token = 'a35794d8563403934c2a3c105b14561ba7f5c2e473450e11a07eb4a5724a4de5cdf4421720bbc6ec028676ba47b99a3b533ce568ce63fa7d581080de801de083'
