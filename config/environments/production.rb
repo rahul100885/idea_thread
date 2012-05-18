56d280a184ece6068b4eb300fb83a34b5f43ab53
Rahul P. Chaudhari <rahul100885@gmail.com>
2012-05-18 23:38:06 +0530

    Production setting for heroku is added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,22 @@ IdeaThread::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # For Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'idea-thread.com',
+    :user_name            => 'rahul.chaudhari@anchanto.com',
+    :password             => '#ahariom123',
+    :authentication       => 'plain',
+    :openssl_verify_mode  => OpenSSL::SSL::VERIFY_NONE,
+  }
 end
